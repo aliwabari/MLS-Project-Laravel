317c3df38d9dda41a0838976296b160f477b29c5
AliWabari <126356541+aliwabari@users.noreply.github.com>
2025-09-05 11:17:43 +0300

    Add Laravel project files

---
+++ database/migrations/2025_09_05_080454_create-student_answers.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('student_answers', function(Blueprint $table){
+            $table->id();
+            $table->foreignId('exam_id')->constrained('exams','id')->cascadeOnDelete();
+            $table->foreignId('question_id')->constrained('questions','id')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users','id')->cascadeOnDelete();
+            $table->foreignId('option_id')->constrained('options','id')->cascadeOnDelete();
+            $table->text('answer')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('student_asnwers');
+    }
+};
